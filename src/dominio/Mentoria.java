edc4d60752859da131a9cda6f928ade252b99f1c
x1ibns <x1ibns@gmail.com>
2023-02-27 19:48:37 -0300

    "second commit"

---
+++ src/dominio/Mentoria.java
@@ -0,0 +1,30 @@
+package dominio;
+
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{
+    private LocalDate data ;
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo = " + getTitulo() +
+                "descricao = " + getDescricao() +
+                "data=" + data +
+                '}';
+    }
+
+    @Override
+    public double calculaXp() {
+        return 0;
+    }
+}
